996ba7c4c3f4e7aef52966d889e5b789d116857a
Süveyda Sultan Altay <suveydaaltay3@gmail.com>
2024-01-08 13:03:46 +0300

    Karar yapıları

---
+++ Karar_Yapıları/Karar_Yapıları/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Karar_Yapıları
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt16(textBox1.Text);
+            if(sayi % 2 == 0)
+            {
+                label1.Text = "Çift";
+            } 
+            else
+            {
+                label1.Text = "Tek";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Urunu implement
+
+        }
+    }
+}
